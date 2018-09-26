8dbe47cc4e83a6eff1f3ac02b8a66d42b988f9d3
samsolsol <he.dupont42@gmail.com>
2018-09-26 17:39:47 +0200

    script fonctionnel sur debian et centos

--- auto_vlan_dhcp.py
+++ auto_vlan_dhcp.py
@@ -6,8 +6,9 @@
 
 #!! Ce programme est à utiliser sur le serveur DHCP !!
 
-import os,sys,re
+import os,sys,re,time
 from fonctions import *
+from operator import *
 
 # Arrêt du programme si l'utilisateur n'est pas en root
 
@@ -16,30 +17,37 @@ from fonctions import *
 #  sys.exit(1)
 
 #else:
-#	print("\n## Bienvenue dans le programme d'automatisation de fourniture d'adresses ##\n")
+print("\n## Bienvenue dans le programme d'automatisation de fourniture d'adresses ##\n")
 	
-choix_distrib = input("\n\nSur quelle type de serveur êtes-vous? Tapez d pour debian ou c pour centos/redhat\n")
+choix_distrib = input("\n\nSur quelle type de serveur êtes-vous? Tapez d pour debian ou c pour centos/redhat\n>>>")
+
+# Verification de l'installation du paquet "vlan"
+if choix_distrib == "d":
+	vlan_paquet = input("Avez-vous installé le paquet vlan 'o' ou 'n' (oui/non) ? \n>>>")
+
+	if vlan_paquet == "n":
+		print("Le paquet vlan va être installé dans quelques secondes !")
+
+		time.sleep(5)
+
+		os.system('apt-get install vlan')
 
 # Validation de l'interface
-
 ok_interface = ""
 
 while ok_interface != "o":
 
 	# Choix de l'interface
-
 	interface = input("\nQuelle interface souhaitez-vous configurer?\n\n"+ get_all_interfaces() + "\n\n>>>")
 	print("\n## Voici l'interface que vous avez renseigné: " + interface + " ##")
-	ok_interface = input("\nVoulez-vous valider cette interface? Tapez o ou n (oui/non)\n")
+	ok_interface = input("\nVoulez-vous valider cette interface? Tapez o ou n (oui/non)\n>>>")
 
 # Validation de l'adresse réseau
-
 ok_network_address = ""
 
 while ok_network_address != "o":
 
 	# Demande de l'adresse du réseau à l'utilisateur
-
 	main_network_address = ""
 	expression = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
@@ -51,17 +59,18 @@ while ok_network_address != "o":
 
 
 # Validation de la liste de VLAN
-
 ok_vlan = ""
+max_ip_address = 0
 
 while ok_vlan != "o":
 
-
 	# Demande du nombre de poste dans chaque VLAN
-
 	nb_vlan = 1
 	nb_ip_address = []
 	netmask = []
+
+	print("\nMerci de classer les vlans du plus grand au plus petit ! \n")
+	time.sleep(5)
 
 	nb_ip_addresses = input("\nCombien de poste de travail souhaitez-vous configurer dans le VLAN 1?\n>>>")
 	nb_ip_addresses = int(nb_ip_addresses)
@@ -75,7 +84,6 @@ while ok_vlan != "o":
 
 
 	# Ajout d'un nouveau VLAN
-
 	while another_vlan == "o":
 		nb_vlan = int(nb_vlan)
 		nb_vlan += 1
@@ -83,6 +91,7 @@ while ok_vlan != "o":
 
 		nb_ip_addresses = input("Combien de poste de travail souhaitez-vous configurer dans le VLAN " + nb_vlan + "?\n>>>")
 		nb_ip_addresses = int(nb_ip_addresses)
+
 		check_nb_ip_address = test_nb_ip_address(nb_ip_addresses)
 		check_netmask = test_netmask(check_nb_ip_address)
 
@@ -92,25 +101,33 @@ while ok_vlan != "o":
 		another_vlan = input("Voulez-vous configurer un autre VLAN? Tapez o ou n (oui/non)?\n>>>")
 
 
-	# Affichage de la liste des VLAN
+	for elt in nb_ip_address:
+		max_ip_address += int(elt)
 
-	print("\n## Suite à l'application des masques de sous-réseaux, voici le nombre de postes disponibles pour chaques VLAN ##\n")
+	if max_ip_address < 235:
 
-	for i, elt in enumerate(nb_ip_address):
-		print("Le VLAN {} sera composé de {} poste(s) de travail.".format((i+1), elt))
+		# Affichage de la liste des VLAN
+		print("\n## Suite à l'application des masques de sous-réseaux, voici le nombre de postes disponibles pour chaques VLAN ##\n")
 
+		# Tri du plus grand au plus petit VLAN
+		#nb_ip_address = sorted(nb_ip_address, reverse=True)
 
-	# Confirmation de la liste de VLAN
-	
-	ok_vlan = input("\nVoulez-vous valider cette sélection? Tapez o ou n (oui/non)\n>>>")
+		for i, elt in enumerate(nb_ip_address):
+			print("Le VLAN {} sera composé de {} poste(s) de travail.".format((i+1), elt))
+		
 
+		# Confirmation de la liste de VLAN
+		ok_vlan = input("\nVoulez-vous valider cette sélection? Tapez o ou n (oui/non)\n>>>")
+
+	else:
+
+		print("\n\n ! ! ! ! Le nombre de postes est trop élevé, merci de recommencer ! ! ! ! \n\n")
+		ok_vlan = "n"
 
 # Insertion de l'adresse de réseau dans une liste pour séparer les octets
-
 split_network_address = main_network_address.split(".")
 
 # Convertion des octets en int
-
 octet_1 = int(split_network_address[0])
 octet_2 = int(split_network_address[1])
 octet_3 = int(split_network_address[2])
@@ -118,7 +135,6 @@ octet_4 = int(split_network_address[3])
 
 
 # Création des tableaux pour les interfaces de sous-réseau
-
 i = 0
 list_subnet = []
 network_address = []
@@ -130,7 +146,6 @@ interface = str(interface)
 nb_vlan = int(nb_vlan)
 
 # Remplissage des tableaux pour les interfaces de sous-réseau
-
 while i < nb_vlan:
 
 	interface_address.append(octet_4 + 1)
@@ -140,28 +155,23 @@ while i < nb_vlan:
 	octet_4 = broadcast_address[i] + 1
 
 	# Fabrication d'une liste des interfaces pour éditer le fichier isc-dhcp-server
-
 	list_subnet.append(interface + "." + str(i + 1))
 
-	# print("auto " + interface + "." + str(i) + \
-	# 	"\niface " + interface + "." + str(i) + " inet static" + \
-	# 	"\naddress " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + \
-	# 	"\nnetmask " + netmask[i] + \
-	# 	"\nnetwork " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + \
-	# 	"\nbroadcast " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i]) + \
-	# 	"\nvlan_raw_device " + interface + "\n")
+	# Ecriture de l'interface principale pour la distrib centos
+	if choix_distrib == "c":
+		write_main_interface(interface)
 
 	# Ecriture du fichier /etc/network/interfaces
-
 	if choix_distrib == "d":
 
 		write_interfaces_debian(interface, i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask)
 
+	# Ecriture dans le dossier /etc/sysconfig/network-scripts
 	else:
-		write_interfaces_centos(i, interface, netmask, octet_1, octet_2, octet_3, interface_address)
+
+		write_interfaces_centos(i, interface, netmask, octet_1, octet_2, octet_3, interface_address, network_address)
 		
 	# Ecriture du fichier /etc/dhcp/dhcpd.conf
-
 	if choix_distrib == "d":
 
 		write_dhcpd_debian(i, octet_1,octet_2, octet_3,interface_address, network_address, broadcast_address, netmask)
@@ -170,25 +180,31 @@ while i < nb_vlan:
 
 		write_dhcpd_centos(i, octet_1,octet_2, octet_3,interface_address, network_address, broadcast_address, netmask)
 
-	# print("\n# VLAN " + str(i + 1) + \
-	# 	"\nsubnet " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + " netmask " + netmask[i] + " {" + \
-	# 	"\n    option routers " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + ";" + \
-	# 	"\n    option broadcast-address " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i]) + ";" + \
-	# 	"\n    range " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i] + 1) + " " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i] - 1) + ";" + \
-	# 	"\n}")
-
 	i += 1
 
-# Ajout de Authoritative à dhcpd.conf
-
+# Ajout dconfig à dhcpd.conf
 write_dhcpd_infos()
 
 # Transformation de ma liste en str
-
 str_list_subnet = " ".join(list_subnet)
 
-# Ecriture du fichier /etc/default/isc-dhcp-server
-
+# Ecriture du fichier /etc/default/isc-dhcp-server + restart network et dhcp
 if choix_distrib == "d":
 
 	write_isc_dhcp_server(str_list_subnet)
+
+	print("La configuration est terminée !!")
+	time.sleep(2)
+	print("Vous devez maintenant relancer le réseau puis le service dhcp")
+	time.sleep(4)
+	print("\n#### Commande network: systemctl restart networking\n" + \
+		"\n#### Commande dhcp: systemctl restart isc-dhcp-server.service\n")
+
+else:
+
+	print("La configuration est terminée !!")
+	time.sleep(2)
+	print("Vous devez maintenant relancer le réseau puis le service dhcp")
+	time.sleep(4)
+	print("\n#### Commande network: systemctl restart network\n" + \
+		"\n#### Commande dhcp: systemctl restart dhcpd\n")
--- fonctions.py
+++ fonctions.py
@@ -8,6 +8,7 @@ import os
 def get_all_interfaces():
 
     list = os.listdir('/sys/class/net/')
+   
     return " ".join(list)
 
 
@@ -33,6 +34,7 @@ def test_nb_ip_address(nb_ip_address):
 
 
 def test_netmask(nb_ip_address):
+	
 	if nb_ip_address == 5:
 		return "255.255.255.248"
 
@@ -52,11 +54,9 @@ def test_netmask(nb_ip_address):
 		return "255.255.255.0"
 
 # Ecriture du fichier /etc/network/interfaces
-
 def write_interfaces_debian(interface, i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
 
 	interfaces_file = open("/etc/network/interfaces","a")
-
 	interfaces_file.write("\nauto " + interface + "." + str(i + 1) + \
 		"\niface " + interface + "." + str(i + 1) + " inet static" + \
 		"\n    address " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + \
@@ -67,29 +67,55 @@ def write_interfaces_debian(interface, i, octet_1, octet_2, octet_3, interface_a
 
 	interfaces_file.close()
 
+# def display_interfaces_debian(interface, i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
+
+# 	print("\nauto " + interface + "." + str(i + 1) + \
+# 		"\niface " + interface + "." + str(i + 1) + " inet static" + \
+# 		"\n    address " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + \
+# 		"\n    netmask " + netmask[i] + \
+# 		"\n    network " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + \
+# 		"\n    broadcast " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i]) + \
+# 		"\n    vlan_raw_device " + interface + "\n")
+
 # Ecriture dans le dossier /etc/sysconfig/network-scripts/
-
-def write_interfaces_centos(i, interface, netmask, octet_1, octet_2, octet_3, interface_address):
-
-	interfaces_file = open("/etc/sysconfig/network-scripts/ifcfg-" + interface + ":" + str(i + 1) , "a")
-
-	interfaces_file.write("\nDEVICE=" + interface + ":" + str(i + 1) + \
+def write_main_interface(interface):
+	
+	interface_file = open("/etc/sysconfig/network-scripts/ifcfg-" + interface, "w")
+	interface_file.write("\nDEVICE=" + interface + \
 		"\nTYPE=Ethernet" + \
-		"\nBOOTPROTO=dhcp" + \
+		"\nBOOTPROTO=none" + \
+		"\nONBOOT=yes")
+	
+	interface_file.close()
+
+
+def write_interfaces_centos(i, interface, netmask, octet_1, octet_2, octet_3, interface_address, network_address):
+	
+	interfaces_file = open("/etc/sysconfig/network-scripts/ifcfg-" + interface + "." + str(i + 1) , "a")
+	interfaces_file.write("\nDEVICE=" + interface + "." + str(i + 1) + \
+		"\nBOOTPROTO=none" + \
+		"\nONBOOT=yes" + \
 		"\nIPADDR=" + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(interface_address[i]) + \
 		"\nNETMASK=" + netmask[i] + \
-		"\nNAME=" + interface + ":" + str(i + 1) + \
-		"\nONBOOT=yes" + \
+		"\nNETWORK=" + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + \
 		"\nVLAN=yes")
 
 	interfaces_file.close()
 
+# def display_interfaces_centos(i, interface, netmask, octet_1, octet_2, octet_3, interface_address, network_address):
+
+# 	print("\nDEVICE=" + interface + "." + str(i + 1) + \
+# 		"\nBOOTPROTO=none" + \
+# 		"\nONBOOT=yes" + \
+# 		"\nIPADDR=" + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(interface_address[i]) + \
+# 		"\nNETMASK=" + netmask[i] + \
+# 		"\nNETWORK=" + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + \
+# 		"\nVLAN=yes")
+
 # Ecriture du fichier /etc/dhcp/dhcpd.conf
-
-def write_dhcpd_debian(i, octet_1,octet_2, octet_3,interface_address, network_address, broadcast_address, netmask):
-
+def write_dhcpd_debian(i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
+	
 	dhcpd_files = open("/etc/dhcp/dhcpd.conf","a")
-
 	dhcpd_files.write("\n# VLAN " + str(i + 1) + \
 		"\nsubnet " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + " netmask " + netmask[i] + " {" + \
 		"\n    option routers " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + ";" + \
@@ -99,12 +125,19 @@ def write_dhcpd_debian(i, octet_1,octet_2, octet_3,interface_address, network_ad
 
 	dhcpd_files.close()
 
+# def display_dhcpd_debian(i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
+
+# 	print("\n# VLAN " + str(i + 1) + \
+# 		"\nsubnet " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + " netmask " + netmask[i] + " {" + \
+# 		"\n    option routers " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + ";" + \
+# 		"\n    option broadcast-address " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i]) + ";" + \
+# 		"\n    range " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i] + 1) + " " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i] - 1) + ";" + \
+# 		"\n}\n")
+
 # Ecriture du fichier /etc/dhcp/dhcpd.conf
-
-def write_dhcpd_centos(i, octet_1,octet_2, octet_3,interface_address, network_address, broadcast_address, netmask):
-
+def write_dhcpd_centos(i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
+	
 	dhcpd_files = open("/etc/dhcp/dhcpd.conf","a")
-
 	dhcpd_files.write("\n\n# VLAN " + str(i + 1) + \
 		"\nsubnet " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + " netmask " + netmask[i] + " {" + \
 		"\n    option routers " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + ";" + \
@@ -114,23 +147,29 @@ def write_dhcpd_centos(i, octet_1,octet_2, octet_3,interface_address, network_ad
 
 	dhcpd_files.close()
 
+# def display_dhcpd_centos(i, octet_1, octet_2, octet_3, interface_address, network_address, broadcast_address, netmask):
+
+# 	print("\n\n# VLAN " + str(i + 1) + \
+# 		"\nsubnet " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(network_address[i]) + " netmask " + netmask[i] + " {" + \
+# 		"\n    option routers " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i]) + ";" + \
+# 		"\n    option broadcast-address " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i]) + ";" + \
+# 		"\n    range " + str(octet_1) + "." + str(octet_2)+ "." + str(octet_3) + "." + str(interface_address[i] + 1) + " " + str(octet_1) + "." + str(octet_2) + "." + str(octet_3) + "." + str(broadcast_address[i] - 1) + ";" + \
+# 		"\n}\n")
+
 # Ajout config à dhcpd.conf
-
 def write_dhcpd_infos():
-
+	
 	dhcpd_files = open("/etc/dhcp/dhcpd.conf","a")
-	dhcpd_files.write("\nauthoritative;\nddns-update-style none;\nmax-lease-time 7200;\ndefault-lease-time 600;")
+	dhcpd_files.write("\noption domain-name-servers 8.8.8.8, 8.8.4.4;\nauthoritative;\nddns-update-style none;\nmax-lease-time 7200;\ndefault-lease-time 600;")
+	
 	dhcpd_files.close()
 
 
 # Ecriture du fichier /etc/default/isc-dhcp-server
-
 def write_isc_dhcp_server(str_list_subnet):
 	
 	isc_dhcp_server_files = open("/etc/default/isc-dhcp-server","w")
 	isc_dhcp_server_files.write("INTERFACES=\"" + str_list_subnet + "\"")
+	
 	isc_dhcp_server_files.close()
-
-
-
 
